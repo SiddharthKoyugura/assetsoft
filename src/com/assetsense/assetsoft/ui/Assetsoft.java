9c4af1979da33f29d0fb767aed807d2d9559e793
SiddharthKoyugra <koyugurasiddhardha@gmail.com>
2024-03-05 16:48:12 +0530

    completed addUpdate page

--- src/com/assetsense/assetsoft/ui/Assetsoft.java
+++ src/com/assetsense/assetsoft/ui/Assetsoft.java
@@ -1,7 +1,10 @@
 package com.assetsense.assetsoft.ui;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class Assetsoft implements EntryPoint {
 
@@ -9,7 +12,20 @@ public class Assetsoft implements EntryPoint {
 	public void onModuleLoad() {
 
 		TaskDashboard taskDashboard = new TaskDashboard();
+		AddEditForm addEditForm = new AddEditForm();
 		
-		RootLayoutPanel.get().add(taskDashboard.getTaskDashboard());
+//		RootLayoutPanel.get().add(taskDashboard.getTaskDashboard());
+		
+		
+		DockLayoutPanel dpanel = new DockLayoutPanel(Unit.PX);
+		VerticalPanel vpanel = new VerticalPanel();
+		vpanel.setWidth("100%");
+		
+		dpanel.addNorth(taskDashboard.buildNavBar(), 50);
+		
+		vpanel.add(addEditForm.buildFormHeader());
+		vpanel.add(addEditForm.buildForm());
+		dpanel.add(vpanel);
+		RootLayoutPanel.get().add(dpanel);
 	}
 }
